400bd02d0d832771273c035c03734e79570acfa3
Artem Gordienko <arking@ukr.net>
2016-09-20 10:19:07 +0300

    Update dbconnect.php

--- server/dbconnect.php
+++ server/dbconnect.php
@@ -2,12 +2,12 @@
 session_start();
 
 $dsn = 'pgsql:'
-    . 'host=ec2-107-21-229-87.compute-1.amazonaws.com;'
-    . 'dbname=d6cs0p001bbf4j;'
-    . 'user=gnxhobringpqjy;'
+    . 'host=ec2-23-21-235-126.compute-1.amazonaws.com;'
+    . 'dbname=d87nh9a60mmm1r;'
+    . 'user=ocxitzuqvihisr;'
     . 'port=5432;'
     . 'sslmode=require;'
-    . 'password=3KuSCO4nBlUZ_EYSk-v_WXzeKs';
+    . 'password=X6p-VelgjUtJG_m6igY5LIh__f';
 
 $db=new PDO($dsn);
 /*
